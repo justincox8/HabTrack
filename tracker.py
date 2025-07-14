f2cda87693c07b788e24331d351621e492345d20
Justin Cox <justincox2710@gmail.com>
2025-07-14 12:38:23 -0700

    added a function to add habits

--- tracker.py
+++ tracker.py
@@ -1,5 +1,15 @@
-import JSON
+import json
 import datetime
 
-def add_habbit():
-    pass
+def add_habbit(habit, desc):
+    with open('habit.json', 'r') as f:
+        habits = json.load(f)
+    new_habit = {len(habits):{
+        "habit":habit,
+        "description":desc,
+        "streak":0
+    }}
+    habits.update(new_habit)
+    with open('habit.json', 'w') as f:
+        json.dump(habits, f, indent=4)
+    
