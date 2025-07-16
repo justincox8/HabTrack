f126483d68877b07d2229987f831fb1dce53f244
Justin Cox <justincox2710@gmail.com>
2025-07-16 09:53:30 -0700

    added a increase streak function and a print habits function; also began loosely working on main

---
+++ main.py
@@ -0,0 +1,8 @@
+import tracker.py
+
+def main():
+    add_habit('coding', 'its super duper fun')
+    print_habits()
+
+
+main()
--- tracker.py
+++ tracker.py
@@ -1,13 +1,21 @@
 import json
 import datetime
 
+def print_habits():
+    habits = {}
+    with open('habit.json', 'r') as f:
+        habits = json.load(f)
+    print("HabTrack\n")
+    for key, value in habits.items():    
+        print(f"{value['habit']}-{value['description']}\nStreak: {value['streak']}\n")
+
 def add_habit(habit, desc):
     try:
         with open('habit.json', 'r') as f:
             habits = json.load(f)
     except json.decoder.JSONDecodeError:
         habits = {}
-    new_habit = {len(habits):{
+    new_habit = {len(habits)+1:{
         "habit":habit,
         "description":desc,
         "streak":0
@@ -15,6 +23,7 @@ def add_habit(habit, desc):
     habits.update(new_habit)
     with open('habit.json', 'w') as f:
         json.dump(habits, f, indent=4)
+    print_habits()
 
 def delete_habit(delhabit):
     habits = {}
@@ -28,3 +37,14 @@ def delete_habit(delhabit):
         del habits[keytodelete]
     with open('habit.json', 'w') as f:
         json.dump(habits, f, indent=4)
+    print_habits()
+
+def increase_streak():
+    habtoincrease = input("Please input the number of the habit you would like to increase: ")
+    habits = {}
+    with open('habit.json', 'r') as f:
+        habits = json.load(f)
+    habits[habtoincrease]['streak'] +=1
+    with open('habit.json', 'w') as f:
+        json.dump(habits, f, indent=4)
+    print_habits()
