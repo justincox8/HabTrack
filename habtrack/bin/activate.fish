652f45ebcfe85241cb6e3cdb0df9d856a67a7bfe
Justin <justincox2710@gmail.com>
2025-12-29 08:00:59 -0800

    fixed slight css bug

--- habtrack/bin/activate.fish
+++ habtrack/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/justin/HabTrack/habtrack
+set -gx VIRTUAL_ENV /home/desktop/HabTrack/habtrack
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
